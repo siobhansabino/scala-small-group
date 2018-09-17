f9f38af7b91722d37ff8022a3e3a56a49f0d527a
Siobhan Sabino <siobhan.sabino@gmail.com>
2018-09-17 15:36:11 -0400

    Set as well based on what list has as examples but modified

--- src/main/scala/groupwork/examples/CollectionOfCollections.scala
+++ src/main/scala/groupwork/examples/CollectionOfCollections.scala
@@ -48,7 +48,57 @@ object CollectionOfCollections {
     println(listTriple)
   }
   
-  def setsExample: Unit = println("\n\nsets: tbd") //todo
+  def setsExample: Unit = {
+    println("\n\n***\tSET EXAMPLE\t***")
+    
+    println("\n\nBasic set creation")
+    val setBasic = Set(1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 8, 9, 7, 3, 2)
+    println(setBasic)
+  
+    println("\n\nStringifying sets")
+    println(setBasic.mkString(", "))
+    println(setBasic.mkString("[", ", ", "]"))
+    
+    println("\n\nList to set")
+    val listWithDuplicates = List(1, 2, 2, 3, 4, 5, 5, 5, 6, 7, 8, 8, 8, 9, 7, 3, 2)
+    println(listWithDuplicates)
+    val setFromList = listWithDuplicates.toSet
+    println(setFromList)
+  
+    println("\n\nBasic set information")
+    println("Size:\t" + setBasic.size)
+    println("Is empty:\t" + setBasic.isEmpty)
+    println("Is non empty:\t" + setBasic.nonEmpty)
+    
+    println("\n\nAdding and removing sets")
+    val setFruit = Set("apples", "oranges", "pears", "tomato")
+    println("Fruit:\t" + setFruit)
+    val setVeg = Set("cucumbers", "broccoli", "cabbage", "tomato")
+    println("Veg:\t" + setVeg)
+    val itemWeirdProduce = "tomato"
+    println("Weird:\t" + itemWeirdProduce)
+    val setAllProduce = setFruit ++ setVeg
+    println("All produce:\t" + setAllProduce)
+    val setAllNotWeirdProduce = setAllProduce - itemWeirdProduce
+    println("All not weird produce:\t" + setAllNotWeirdProduce)
+    val setCommonProduce = setFruit.intersect(setVeg)
+    println("All common produce:\t" + setCommonProduce)
+    val setNotCommonProduce = setFruit.diff(setVeg) ++ setVeg.diff(setFruit)
+    println("All not common produce:\t" + setNotCommonProduce)
+  
+    println("\n\nTake and drop")
+    println("Full:\t" + setAllProduce)
+    println("Take 1:\t" + setAllProduce.take(1))
+    println("Drop 1:\t" + setAllProduce.drop(1))
+    
+    println("\n\nHigher order functions")
+    println("Filter to keep only even elements:")
+    val setEven = setBasic.filter(element => element % 2 == 0)
+    println(setEven)
+    println("Map to triply all elements:")
+    val setTriple = setBasic.map(element => element * 3)
+    println(setTriple)
+  }
   
   def mapsExample: Unit = println("\n\nmaps: tbd") //todo
   
